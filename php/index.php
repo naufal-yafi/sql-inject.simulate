bba024823f4fffc7fb663d04f7197238ba14885b
naufal-yafi <muhammadnaufalyafi00@gmail.com>
2024-11-28 01:43:48 +0000

    change path styles and logo

--- php/index.php
+++ php/index.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SQL Injection Demo</title>
-    <link rel="stylesheet" href="/public/styles/homepage.css">
-    <link rel="shortcut icon" href="/public/image/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="/styles/homepage.css">
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
 </head>
 <body>
     <header class="text__group">
